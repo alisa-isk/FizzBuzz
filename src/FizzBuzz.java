d77bae53c5b5a8a89c63376f364e2c6852ea377d
Alisa <157826128+alisa-isk@users.noreply.github.com>
2024-09-09 14:24:01 -0400

    steps up to 3.3

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -11,7 +11,7 @@ class FizzBuzz {
         }
     }
 
-    private static int doFizzBuzz(int i) {
+    public static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
@@ -37,5 +37,8 @@ class FizzBuzz {
         }
         i++;
         return i;
+    }
+
+    public static void main() {
     }
 }
